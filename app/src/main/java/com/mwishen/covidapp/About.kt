b8ef188ba741a397bfcae16ae4b717e271e4f845
modcomlearning <modcomlearning@gmail.com>
2021-07-22 13:36:50 +0300

    Second Commit

--- app/src/main/java/com/mwishen/covidapp/About.kt
+++ app/src/main/java/com/mwishen/covidapp/About.kt
@@ -2,9 +2,15 @@ package com.mwishen.covidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+
 class About : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val webabout = findViewById<WebView>(R.id.webabout)
+        webabout.loadUrl("file:///android_asset/about.html")
+        //Next we go to android assets folder and put the html there
     }
 }
--- app/src/main/java/com/mwishen/covidapp/Symptoms.kt
+++ app/src/main/java/com/mwishen/covidapp/Symptoms.kt
@@ -2,10 +2,14 @@ package com.mwishen.covidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class Symptoms : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_symptoms)
+
+        val websymptoms = findViewById<WebView>(R.id.websymptoms)
+        websymptoms.loadUrl("file:///android_asset/symptoms.html")
     }
 }
